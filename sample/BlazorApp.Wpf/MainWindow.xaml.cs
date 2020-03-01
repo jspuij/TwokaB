fa80102a19786336da03b8aaf7e05c28a8486765
Jan-Willem Spuij <jw@spuij.nl>
2020-03-01 12:21:41 +0100

    Moved source files. Added WPF version.

---
+++ sample/BlazorApp.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebWindows.Blazor;
+
+namespace BlazorApp.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IDisposable run;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComponentsDesktop.Run<Startup>(this.BlazorWebView, "wwwroot/index.html");
+        }
+    }
+}
---
+++ src/BlazorWebView.Wpf/BlazorWebView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BlazorWebView.Wpf
+{
+    public class BlazorWebView : FrameworkElement,  IBlazorWebView
+    {
+        public event EventHandler<string> OnWebMessageReceived;
+
+        public void Initialize(Action<WebViewOptions> configure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Invoke(Action callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMessage(string title, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
